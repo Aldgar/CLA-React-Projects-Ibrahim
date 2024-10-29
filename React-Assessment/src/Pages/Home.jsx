1c825b4a123ae5d814cd509838aad3d644d44e88
Aldegar <97394969+Aldgar@users.noreply.github.com>
2024-10-29 20:01:26 +0000

    the first assesment of react coder challange

---
+++ React-Assessment/src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+function Home() {
+  return (
+    <>
+    <Navbar /> 
+    <div>
+       
+    </div>
+    </>
+  )
+}
+
+export default Home
